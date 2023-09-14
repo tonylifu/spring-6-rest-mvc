cbce91e39c2a0470bef1c3b030ade97bf12d786a
Anthony Ohiero Lifu <tonylifu@outlook.com>
2023-09-14 15:38:34 +0100

    first commit

---
+++ src/main/java/com/wtk/test/SpringMvc6WebappApplication.java
@@ -0,0 +1,13 @@
+package com.wtk.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvc6WebappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvc6WebappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wtk/test/SpringMvc6WebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wtk.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvc6WebappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
